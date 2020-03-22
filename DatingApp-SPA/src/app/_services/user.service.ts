49289ffcd23e3453cea2a90c1dea2ce4a7559afd
Bleron Qorri <bleron.qorri@beriflapp.com>
2020-03-22 19:12:25 +0100

    End of Section 9

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+
+  // tslint:disable-next-line: variable-name
+  constructor(private _httpClient: HttpClient) { }
+
+
+  getUsers(): Observable<User[]> {
+    return this._httpClient.get<User[]>(this.baseUrl + 'users');
+  }
+
+  getUser(id: number): Observable<User> {
+    return this._httpClient.get<User>(this.baseUrl + 'users/' + id);
+  }
+}
+

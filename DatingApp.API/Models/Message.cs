a676ef5981ae0ef87a08ecda160ef99cb59e0119
Bleron Qorri <bleron.qorri@beriflapp.com>
2020-03-26 17:52:16 +0100

    Pre-Publishing

--- DatingApp.API/Models/Message.cs
+++ DatingApp.API/Models/Message.cs
@@ -8,10 +8,10 @@ namespace DatingApp.API.Models
 
         public int SenderId { get; set; }
 
-        public User Sender { get; set; }
+        public virtual User Sender { get; set; }
         public int RecipientId { get; set; }
 
-        public User Recipient { get; set; }
+        public virtual User Recipient { get; set; }
 
         public string Content {get; set;}
 
